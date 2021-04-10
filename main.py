871e397cab6e08350c2ea6e00e91c8429ca9baed
DARKPASSION <>
2021-04-10 14:57:14 +0000

    Comments  and api adjustment

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import discord
 import os
+#brings in https calls
 import requests
 import json
 import random
-from replit import db
+
 from keep_alive import keep_alive
 
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
-
+# word scanner list
+sad_words = [
+  "sad", 
+  "depressed", 
+  "unhappy", 
+  "angry", 
+  "miserable", 
+  "depressing"
+]
+# replies to encouragements
 encouragements = [
   "Cheer up!",
   "Hang in there.",
-  "You are a great person / bot!"
+  "You are a great person!"
 ]
 
-if "responding" not in db.keys():
-  db["responding"] = True
 
+# http get
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
+  # translare json
   json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  quote =  json_data[0]['a'] + " just once said " + json_data[0]['q']
   return(quote)
 
 
@@ -36,13 +45,15 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+  # shorthandle 
   msg = message.content
 
+  # random quotes by command
   if msg.startswith('$inspire'):
     quote = get_quote()
-
     await message.channel.send(quote)
 
+  # word scanner
   if any(word in msg for word in sad_words):
       await message.channel.send(random.choice(encouragements))
 
