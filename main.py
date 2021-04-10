ea7bf68642c81d11f5b9f841256d1234ac343ae1
DARKPASSION <>
2021-04-10 14:29:49 +0000

    In

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+import os
+import requests
+import json
+import random
+from replit import db
+from keep_alive import keep_alive
+
+client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith('$inspire'):
+    quote = get_quote()
+
+    await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(starter_encouragements))
+
+
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
